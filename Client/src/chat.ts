d3451a02865e13bf462f1e41085e3622321c760e
butthead <butthead.prod@gmail.com>
2026-05-25 00:55:07 +0400

    send messages on Enter. one button for connections

--- Client/src/chat.ts
+++ Client/src/chat.ts
@@ -1,183 +1,195 @@
 import { User, UserStates } from "./user";
 
 interface ClientConnectRequest {
-    type: string;
-    username: string;
-    message: string;
+	type: string;
+	username: string;
+	message: string;
 }
 
 interface ClientRequest {
-    type: string;
-    message: string;
-    uuid?: string;
+	type: string;
+	message: string;
+	uuid?: string;
 }
 
 interface Static {
-    type: string;
-    uuid: string;
-    server: string;
+	type: string;
+	uuid: string;
+	server: string;
 }
 
 interface ServerResponse {
-    type: string;
-    message: string;
-    username: string;
-    timestamp: number;
-    staticFiles?: Static;
+	type: string;
+	message: string;
+	username: string;
+	timestamp: number;
+	staticFiles?: Static;
 }
 interface ChatHistory {
-    chatHistory: Array<ServerResponse>;
+	chatHistory: Array<ServerResponse>;
 }
 
 export class Chat {
-    private static CHATAREA: HTMLDivElement = document.getElementById("chat_area") as HTMLDivElement;
-    private static readonly DEFAULT_URI: string = "ws://" + location.hostname + ":8080/ws";
+	private static CHATAREA: HTMLDivElement = document.getElementById("chat_area") as HTMLDivElement;
+	private static readonly DEFAULT_URI: string = "ws://" + location.hostname + ":8080/ws";
 
-    private socket?: WebSocket;
-    private user: User;
+	private socket?: WebSocket;
+	private user: User;
 
-    constructor(user: User) {
-        this.user = user;
-    }
+	constructor(user: User) {
+		this.user = user;
+	}
 
-    private renderMessage(message: ServerResponse) {
-        const datetimeLocale = new Date(message.timestamp).toLocaleString();
-        switch (message.type) {
-            case "systemMessage":
-                const systemMessage: string =
-                    "[" + datetimeLocale + "] " + "[" + message.username + "]: " + message.message;
-                this.updateChatField("red", systemMessage);
-                break;
-            case "userMessage":
-                if (message.staticFiles) {
-                    const staticFiles = message.staticFiles;
-                    const userMessage: string =
-                        "[" + datetimeLocale + "] " + "[" + message.username + "]: " + message.message + "\n";
-                    this.updateChatField(
-                        "black",
-                        userMessage,
-                        "http://" + staticFiles.server + "/static/images/" + staticFiles.uuid + ".png",
-                    );
-                } else {
-                    const userMessage: string =
-                        "[" + datetimeLocale + "] " + "[" + message.username + "]: " + message.message;
-                    this.updateChatField("black", userMessage);
-                }
-                break;
-            default:
-                break;
-        }
-    }
+	private renderMessage(message: ServerResponse) {
+		const datetimeLocale = new Date(message.timestamp).toLocaleString();
+		switch (message.type) {
+			case "systemMessage":
+				const systemMessage: string =
+					"[" + datetimeLocale + "] " + "[" + message.username + "]: " + message.message;
+				this.updateChatField("red", systemMessage);
+				break;
+			case "userMessage":
+				if (message.staticFiles) {
+					const staticFiles = message.staticFiles;
+					const userMessage: string =
+						"[" + datetimeLocale + "] " + "[" + message.username + "]: " + message.message + "\n";
+					this.updateChatField(
+						"black",
+						userMessage,
+						"http://" + staticFiles.server + "/static/images/" + staticFiles.uuid + ".png",
+					);
+				} else {
+					const userMessage: string =
+						"[" + datetimeLocale + "] " + "[" + message.username + "]: " + message.message;
+					this.updateChatField("black", userMessage);
+				}
+				break;
+			default:
+				break;
+		}
+	}
 
-    private updateChatField(color: string, message: string, url?: string) {
-        const messageDiv = document.createElement("p");
-        messageDiv.style.color = color;
-        messageDiv.innerText = message;
+	private updateChatField(color: string, message: string, url?: string) {
+		const messageDiv = document.createElement("p");
+		messageDiv.style.color = color;
+		messageDiv.innerText = message;
 
-        if (url) {
-            const messageImg = new Image();
-            messageImg.src = url;
-            messageDiv.appendChild(messageImg);
-        }
+		if (url) {
+			const messageImg = new Image();
+			messageImg.src = url;
+			messageDiv.appendChild(messageImg);
+		}
 
-        Chat.CHATAREA.appendChild(messageDiv);
-    }
+		Chat.CHATAREA.appendChild(messageDiv);
+	}
 
-    private renderChatHistory(history: ChatHistory) {
-        for (const message of history.chatHistory) {
-            this.renderMessage(message);
-        }
-    }
+	private renderChatHistory(history: ChatHistory) {
+		for (const message of history.chatHistory) {
+			this.renderMessage(message);
+		}
+	}
 
-    private clearChat() {
-        Chat.CHATAREA.innerHTML = "";
-    }
+	private clearChat() {
+		Chat.CHATAREA.innerHTML = "";
+	}
 
-    public connect(uri: string) {
-        if (this.user.isUserConnected()) {
-            alert("You already connected to this room!");
-            return;
-        }
+	public isConnect() {
+		return this.user.isUserConnected();
+	}
 
-        if (uri === "") {
-            uri = Chat.DEFAULT_URI;
-        }
+	public connect(uri: string): Promise<boolean> {
+		return new Promise((resolve, reject) => {
+			if (this.user.isUserConnected()) {
+				alert("You already connected to this room!");
+				resolve(true);
+				return true;
+			}
 
-        this.socket = new WebSocket(uri);
+			if (uri === "") {
+				uri = Chat.DEFAULT_URI;
+			}
 
-        this.socket.onopen = () => {
-            this.sendClientConnectRequest("connect", this.user.getUsername(), "connect!");
-            this.user.updateUserState(UserStates.CONNECTED);
-        };
+			this.socket = new WebSocket(uri);
 
-        this.socket.onmessage = (event: MessageEvent) => {
-            const message = JSON.parse(event.data);
-            switch (message.type) {
-                case "chatHistory":
-                    this.renderChatHistory(message as ChatHistory);
-                    break;
-                default:
-                    this.renderMessage(message as ServerResponse);
-                    break;
-            }
-        };
+			this.socket.onopen = () => {
+				this.sendClientConnectRequest("connect", this.user.getUsername(), "connect!");
+				this.user.updateUserState(UserStates.CONNECTED);
+				resolve(true);
+				return true;
+			};
 
-        this.socket.onerror = (event: Event) => {
-            console.log(event);
-        };
+			this.socket.onmessage = (event: MessageEvent) => {
+				const message = JSON.parse(event.data);
+				switch (message.type) {
+					case "chatHistory":
+						this.renderChatHistory(message as ChatHistory);
+						break;
+					default:
+						this.renderMessage(message as ServerResponse);
+						break;
+				}
+			};
 
-        this.socket.onclose = () => {
-            this.user.updateUserState(UserStates.DISCONNECTED);
-            this.clearChat();
-        };
-    }
+			this.socket.onerror = (event: Event) => {
+				console.log(event);
+				reject(false);
+				return false;
+			};
 
-    public disconnect() {
-        if (!this.socket) {
-            return;
-        }
-        this.sendClientRequest("disconnect", "disconnect");
-        this.socket.close();
-    }
+			this.socket.onclose = () => {
+				this.user.updateUserState(UserStates.DISCONNECTED);
+				this.clearChat();
+			};
 
-    public sendClientRequest(type: string, message: string, uuid?: string) {
-        if (!this.socket) {
-            console.log("Socket is undefined");
-            return;
-        }
+		});
+	}
 
-        const userMessage: ClientRequest = {
-            type: type,
-            message: message,
-            uuid: uuid,
-        };
-        const jsonString = JSON.stringify(userMessage);
-        this.socket.send(jsonString);
-    }
+	public disconnect() {
+		if (!this.socket) {
+			return;
+		}
+		this.sendClientRequest("disconnect", "disconnect");
+		this.socket.close();
+	}
 
-    public sendClientImage(image: ArrayBuffer) {
-        if (!this.socket) {
-            console.log("Socket is undefined");
-            return;
-        }
-        this.socket.send(image);
-    }
+	public sendClientRequest(type: string, message: string, uuid?: string) {
+		if (!this.socket) {
+			console.log("Socket is undefined");
+			return;
+		}
 
-    public sendClientConnectRequest(type: string, username: string, message: string) {
-        if (!this.socket) {
-            console.log("Socket is undefined");
-            return;
-        }
-        const userMessage: ClientConnectRequest = {
-            type: type,
-            username: username,
-            message: message,
-        };
-        const jsonString = JSON.stringify(userMessage);
-        this.socket.send(jsonString);
-    }
+		const userMessage: ClientRequest = {
+			type: type,
+			message: message,
+			uuid: uuid,
+		};
+		const jsonString = JSON.stringify(userMessage);
+		this.socket.send(jsonString);
+	}
 
-    public getDefaultUri() {
-        return Chat.DEFAULT_URI;
-    }
+	public sendClientImage(image: ArrayBuffer) {
+		if (!this.socket) {
+			console.log("Socket is undefined");
+			return;
+		}
+		this.socket.send(image);
+	}
+
+	public sendClientConnectRequest(type: string, username: string, message: string) {
+		if (!this.socket) {
+			console.log("Socket is undefined");
+			return;
+		}
+		const userMessage: ClientConnectRequest = {
+			type: type,
+			username: username,
+			message: message,
+		};
+		const jsonString = JSON.stringify(userMessage);
+		this.socket.send(jsonString);
+	}
+
+	public getDefaultUri() {
+		return Chat.DEFAULT_URI;
+	}
 }
--- Client/src/main.ts
+++ Client/src/main.ts
@@ -2,56 +2,71 @@ import { Chat } from "./chat";
 import { User } from "./user";
 
 interface ImageUpload {
-    uuid: string;
+	uuid: string;
 }
 
 function main() {
-    const user: User = new User();
-    const chat: Chat = new Chat(user);
+	const user: User = new User();
+	const chat: Chat = new Chat(user);
 
-    // (document.getElementById("ws_uri") as HTMLInputElement).placeholder = chat.getDefaultUri();
-    (document.getElementById("username") as HTMLInputElement).placeholder = user.getDefaultUsername();
+	(document.getElementById("username") as HTMLInputElement).placeholder = user.getDefaultUsername();
 
-    document.getElementById("confirm_username_change")?.addEventListener("click", () => {
-        const username: string = (document.getElementById("username") as HTMLInputElement).value;
-        user.changeUsername(username);
-    });
+	document.getElementById("confirm_username_change")?.addEventListener("click", () => {
+		const username: string = (document.getElementById("username") as HTMLInputElement).value;
+		user.changeUsername(username);
+	});
 
-    document.getElementById("chat_connect")?.addEventListener("click", () => {
-        chat.connect(chat.getDefaultUri());
-    });
+	document.getElementById("chat_connect")?.addEventListener("click", async (e) => {
+		let target = e.currentTarget as HTMLButtonElement;
+		if (!chat.isConnect()) {
+			let res = await chat.connect(chat.getDefaultUri()).catch(() => { });
+			if (!res) return;
+			target.innerText = "Disconnect";
+		} else {
+			chat.disconnect();
+			target.innerText = "Connect";
+		}
+	});
 
-    document.getElementById("chat_disconnect")?.addEventListener("click", () => {
-        chat.disconnect();
-        alert("You was disconnected!");
-    });
+	document.getElementById("chat_submit_send_message")?.addEventListener("click", async () => {
+		chatSubmitMessage();
+	});
 
-    document.getElementById("chat_submit_send_message")?.addEventListener("click", async () => {
-        const userMessage: string = (document.getElementById("chat_input") as HTMLInputElement).value;
-        const files = (document.getElementById("image_field") as HTMLInputElement).files;
+	document.getElementById('chat_input')?.addEventListener('keydown', (e) => {
+		// Отправка сообщений при нажатии enter в поле ввода
+		if (e.key === 'Enter') {
+			e.preventDefault();
+			chatSubmitMessage();
+		}
+	});
 
-        if (files && files.length > 0) {
-            const file = files[0];
+	function chatSubmitMessage() {
+		const userMessage: string = (document.getElementById("chat_input") as HTMLInputElement).value;
+		const files = (document.getElementById("image_field") as HTMLInputElement).files;
 
-            fetch("//" + location.hostname + ":8080/upload", {
-                method: "Post",
-                headers: { "Content-Type": file.type || "application/octet-stream" },
-                body: file,
-            })
-                .then((response) => response.json())
-                .then((json) => {
-                    const uuid: string = (json as ImageUpload).uuid;
-                    chat.sendClientRequest("userMessage", userMessage, uuid);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        } else {
-            chat.sendClientRequest("userMessage", userMessage);
-        }
-        (document.getElementById("chat_input") as HTMLInputElement).value = "";
-        (document.getElementById("image_field") as HTMLInputElement).value = "";
-    });
+		if (files && files.length > 0) {
+			const file = files[0];
+
+			fetch("//" + location.hostname + ":8080/upload", {
+				method: "Post",
+				headers: { "Content-Type": file.type || "application/octet-stream" },
+				body: file,
+			})
+				.then((response) => response.json())
+				.then((json) => {
+					const uuid: string = (json as ImageUpload).uuid;
+					chat.sendClientRequest("userMessage", userMessage, uuid);
+				})
+				.catch((error) => {
+					console.error(error);
+				});
+		} else {
+			chat.sendClientRequest("userMessage", userMessage);
+		}
+
+		(document.getElementById("chat_input") as HTMLInputElement).value = "";
+		(document.getElementById("image_field") as HTMLInputElement).value = "";
+	}
 }
 
 main();
